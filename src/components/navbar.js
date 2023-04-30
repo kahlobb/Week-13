69270eb5d4d067098fb3e8cc6ac8a5a0701e3343
lobbs <lobbs@lobbss-Air-2.attlocal.net>
2023-04-30 00:59:48 -0500

    update to NavBar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -17,10 +17,7 @@ export default class NavBar extends React.Component {
                             <a class="nav-link" href="#">Features</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Pricing</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link disabled" href="#">Disabled</a>
+                            <a class="nav-link" href="#">Contact Us</a>
                         </li>
                     </ul>
                 </div>
